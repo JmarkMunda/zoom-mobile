de26d0763d84ff5d5984c89d52c46f3b8be44dab
poopijay <jaymarkmunda26@gmail.com>
2024-09-08 18:25:16 +0800

    chore: initial commit

---
+++ config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  sessionName: "DemoSession",
+  roleType: "1",
+  sessionPassword: "",
+  displayName: "Ekaansh",
+  sessionIdleTimeoutMins: 10,
+};
+
+export const ZOOM_APP_KEY = "";
+export const ZOOM_APP_SECRET = "";
